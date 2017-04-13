a83f88a1320fc1df2f6eec72b7905743d1581fbd
Akash Tomar <akash.tomar217@gmail.com>
2017-04-13 19:41:42 +0530

    take pics

--- sensor_camera.py
+++ sensor_camera.py
@@ -3,8 +3,14 @@ from picamera import PiCamera
 
 camera = PiCamera()
 pir = MotionSensor(4)
+camera.capture()
 while True:
-    pir.wait_for_motion()
     camera.start_preview()
-    pir.wait_for_no_motion()
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image.jpg')
     camera.stop_preview()
+
+#    pir.wait_for_motion()
+#    camera.start_preview()
+#    pir.wait_for_no_motion()
+#    camera.stop_preview()
