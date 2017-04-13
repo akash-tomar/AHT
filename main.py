19302d6b941ab73b54129f32c48b610dd3c47fbf
akash tomar <akash.tomar217@gmail.com>
2017-04-13 14:48:39 +0000

    changes in camera and main

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from picamera import PiCamera
 camera = PiCamera()
 pir = MotionSensor(4)
 import time
+count=0
 while True:
     if pir.motion_detected:
-    	camera.capture('/home/pi/Desktop/image.jpg')
-	time.sleep(5)
+	print "motion detected"
+	count+=1
+	name="/home/pi/Desktop/image"+str(count)+".jpg"
+    	camera.capture(name)
+	time.sleep(1)
+    else:
+	print "no motion"
