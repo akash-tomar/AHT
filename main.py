0dcea45d3d31388194a1ace18b9a2ce4806edbc0
Akash Tomar <akash.tomar217@gmail.com>
2017-04-15 17:38:07 +0530

    final logic for possting images

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from gpiozero import MotionSensor
 from picamera import PiCamera
+import os,sys
+import Image
+import requests
 
 camera = PiCamera()
 pir = MotionSensor(4)
 import time
 count=0
 while True:
-    if pir.motion_detected:
-	print "motion detected"
-	count+=1
-	name="/home/pi/Desktop/image"+str(count)+".jpg"
-    	camera.capture(name)
-	time.sleep(1)
-    else:
-	print "no motion"
+	if pir.motion_detected:
+		print "motion detected"
+		count+=1
+		name="/home/pi/Desktop/image"+str(count)+".jpg"
+		camera.capture(name)
+		jpgfile = Image.open(name)
+		print jpgfile
+		sendRequest(jpgfile)
+		time.sleep(3)
+	else:
+		print "no motion"
+
+def sendRequest(image):
+	url = 'https://antihta.herokuapp.com/getResponse/'
+	params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
+	# url="http://127.0.0.1:8000/getResponse/"
+	files = {'media': open(image, 'rb')}
+	r = requests.post(url, data = {'key':'value'},files=files)
