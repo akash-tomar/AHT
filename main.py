769f264dcd0631abc79a66a48ea895125638462d
Akash Tomar <akash.tomar217@gmail.com>
2017-04-15 20:12:57 +0530

    added multiuser support

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def sendRequest(image):
 	params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
 	# url="http://127.0.0.1:8000/getResponse/"
 	files = {'media': open(image, 'rb')}
-	r = requests.post(url, data = {'key':'value'},files=files)
+	r = requests.post(url, data = {'id':'2507'},files=files)
 	print "image sent to fb"
 
 
