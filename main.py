6ab25a2b96155f1afd5b674dadd8d779bb16b1b9
Akash Tomar <akash.tomar217@gmail.com>
2017-04-15 17:41:38 +0530

    changes

--- main.py
+++ main.py
@@ -27,3 +27,4 @@ def sendRequest(image):
 	# url="http://127.0.0.1:8000/getResponse/"
 	files = {'media': open(image, 'rb')}
 	r = requests.post(url, data = {'key':'value'},files=files)
+	print "image sent to fb"
