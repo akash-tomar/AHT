967715230036541a8576fd6b9963e5b99ab3e154
Akash Tomar <akash.tomar217@gmail.com>
2017-04-23 20:23:15 +0530

    added shutdown test code for pi

---
+++ pi_kill.py
@@ -0,0 +1,11 @@
+import requests
+def sendRequest():
+	url = 'https://antihta.herokuapp.com/kill/?id=2507'
+	params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
+	# url = 'http://127.0.0.1:8000/kill/?id=2507'
+	# url="http://127.0.0.1:8000/getResponse/"
+	# files = {'media': open(image, 'rb')}
+	r = requests.get(url)
+	import pdb; pdb.set_trace()
+	print "image sent to fb"
+sendRequest()
---
+++ sockets.py
@@ -0,0 +1,9 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = "ws://127.0.0.1" # Get local machine name
+port = 8000                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
