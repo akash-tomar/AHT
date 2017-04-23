d5c7b9431ac2a0871c73d76a7a0e341af4361352
Akash Tomar <akash.tomar217@gmail.com>
2017-04-23 20:22:55 +0530

    added shutdown test code for pi

--- request.py
+++ request.py
@@ -1,8 +1,8 @@
 import requests
 
-# url = 'https://antihta.herokuapp.com/getResponse/'
-# params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
-url="http://127.0.0.1:8000/getResponse/"
+url = 'https://antihta.herokuapp.com/getResponse/'
+params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
+# url="http://127.0.0.1:8000/getResponse/"
 files = {'media': open('akash.jpg', 'rb')}
 r = requests.post(url, data = {'id':'2507'},files=files)
 import pdb; pdb.set_trace()
