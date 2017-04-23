1b0969d1703789644a61993bd7fd707345c0ca5d
Akash Tomar <akash.tomar217@gmail.com>
2017-04-23 20:22:43 +0530

    added shutdown code for pi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from picamera import PiCamera
 # import os,sys
 # from PIL import Image
 import requests
-
+import json
 camera = PiCamera()
 pir = MotionSensor(4)
 import time
@@ -16,6 +16,27 @@ def sendRequest(image):
 	files = {'media': open(image, 'rb')}
 	r = requests.post(url, data = {'id':'2507'},files=files)
 	print "image sent to fb"
+
+
+def shutDown():
+    command = "sudo poweroff"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print output
+
+def getRequest():
+	url = 'https://antihta.herokuapp.com/kill/?id=2507'
+	params = {"hub.verify_token":"AntiHomeTheft","hub.challenge":"42"}
+	# url = 'http://127.0.0.1:8000/kill/?id=2507'
+	# url="http://127.0.0.1:8000/getResponse/"
+	# files = {'media': open(image, 'rb')}
+	r = requests.get(url)
+	print "pi shut down request"
+	kill = json.loads(r._content)
+	if kill:
+		#shut down pi
+		shutDown()
 
 
 while True:
